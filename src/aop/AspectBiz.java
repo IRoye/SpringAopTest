b48a626c78890dd0a54f4497b192aee3ca7ca1b9
roye <1755431269@qq.com>
2015-12-17 11:49:05 +0800

    AspectBiz.java
    业务类

---
+++ src/aop/AspectBiz.java
@@ -0,0 +1,13 @@
+package aop;
+
+/**
+ * Created by ParanoidRoye on 2015/12/17.
+ * 要实现的业务类
+ */
+public class AspectBiz {
+
+    //业务方法
+    public void biz(){
+        System.out.println("Aspect Biz");
+    }
+}
